75ef9e49ba9d784efe2348fe0073cd66f95df3c3
Tihomir Mihaylov <tihomir_mihaylov@abv.bg>
2023-08-28 22:18:00 +0300

    fix project startup

--- HumanCapital/Program.cs
+++ HumanCapital/Program.cs
@@ -71,7 +71,6 @@ namespace HumanCapital
             app.UseRouting();
 
             app.UseAuthentication();
-            app.UseIdentityServer();
             app.UseAuthorization();
 
             app.MapControllerRoute(
